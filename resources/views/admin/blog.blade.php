be275743e304e747d7b27a3c52eb2db8ab13135c
ahsani3 <ahsaniweb@gmail.com>
2023-04-14 08:41:33 +0700

    update

---
+++ resources/views/admin/blog.blade.php
@@ -0,0 +1,5 @@
+@extends('admin/layouts/main')
+
+@section('content')
+    <h4 class="page-title">{{$title}}</h4>
+@endsection
---
+++ resources/views/admin/home.blade.php
@@ -0,0 +1,7 @@
+@extends('admin/layouts/main')
+
+@section('content')
+	<h4 class="page-title">{{ $title }}</h4>
+	
+	
+@endsection
