59f24bd016cd603dac4003f366f4c596c6f6e0aa
ahsani3 <ahsaniweb@gmail.com>
2023-04-14 08:50:39 +0700

    update

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('/', function () {
+    return view('index', [
+        "title" => "Home"
+    ]);
+});
+
 Route::get('/admin', function () {
     return view('admin/home', [
         "title" => "Dashboard"
