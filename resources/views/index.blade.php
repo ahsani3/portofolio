245ebba9ec11eedd41193ab82dc65ad17334cdfc
ahsani3 <ahsaniweb@gmail.com>
2023-04-14 09:02:18 +0700

    update

---
+++ resources/views/index.blade.php
@@ -0,0 +1,44 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+	<link rel="stylesheet" href="/admin_assets/css/bootstrap.min.css">
+
+    <title>Portofolio | </title>
+  </head>
+  <body>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <a class="navbar-brand" href="/">Portofolio</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="/">Home</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Link</a>
+      </li>
+    </ul>
+    <form class="form-inline my-2 my-lg-0">
+      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+    </form>
+  </div>
+</nav>
+
+ <div class="container">
+    <h3 class="mt-3">Halaman Home</h3>
+ </div>
+
+	<script src="/admin_assets/js/core/jquery.3.2.1.min.js"></script>
+	<script src="/admin_assets/js/core/popper.min.js"></script>
+	<script src="/admin_assets/js/core/bootstrap.min.js"></script>
+  </body>
+</html>
